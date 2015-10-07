3635c87f8208e35472f5ace4cf6a8d4f9c81cd59
Michał 'rysiek' Woźniak <rysiek@hackerspace.pl>
2015-10-07 17:13:34 +0200

    bugfixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,20 +23,36 @@ else
     unset -v ZABBIX_DBHOST ZABBIX_DBPORT ZABBIX_DBNAME ZABBIX_DBUSER ZABBIX_DBPASSWORD
     # let's get db connection data from the config file
     echo "retrieving database config"
-    ZABBIX_DBHOST="$( egrep '^DBHost=.+$' /etc/zabbix/zabbix_server.conf | sed -r -e 's/^DBHost=(.+)$/\1/' )"
-    ZABBIX_DBPORT="$( egrep '^DBPort=.+$' /etc/zabbix/zabbix_server.conf | sed -r -e 's/^DBPort=(.+)$/\1/' )"
-    ZABBIX_DBNAME="$( egrep '^DBName=.+$' /etc/zabbix/zabbix_server.conf | sed -r -e 's/^DBName=(.+)$/\1/' )"
-    ZABBIX_DBUSER="$( egrep '^DBUser=.+$' /etc/zabbix/zabbix_server.conf | sed -r -e 's/^DBUser=(.+)$/\1/' )"
-    ZABBIX_DBPASSWORD="$( egrep '^DBPassword=.+$' /etc/zabbix/zabbix_server.conf | sed -r -e 's/^DBPassword=(.+)$/\1/' )"
+    
+    TMPVAR="$( egrep '^DBHost=.+$' /etc/zabbix/zabbix_server.conf )"
+    [ -z "$TMPVAR" ] || ZABBIX_DBHOST="$( echo "$TMPVAR" | sed -r -e 's/^DBHost=(.+)$/\1/' )"
+    
+    TMPVAR="$( egrep '^DBPort=.+$' /etc/zabbix/zabbix_server.conf )"
+    [ -z "$TMPVAR" ] || ZABBIX_DBPORT="$( echo "$TMPVAR" | sed -r -e 's/^DBPort=(.+)$/\1/' )"
+    
+    TMPVAR="$( egrep '^DBName=.+$' /etc/zabbix/zabbix_server.conf )"
+    [ -z "$TMPVAR" ] || ZABBIX_DBNAME="$( echo "$TMPVAR" | sed -r -e 's/^DBName=(.+)$/\1/' )"
+    
+    TMPVAR="$( egrep '^DBUser=.+$' /etc/zabbix/zabbix_server.conf )"
+    [ -z "$TMPVAR" ] || ZABBIX_DBUSER="$( echo "$TMPVAR" | sed -r -e 's/^DBUser=(.+)$/\1/' )"
+    
+    TMPVAR="$( egrep '^DBPassword=.+$' /etc/zabbix/zabbix_server.conf )"
+    [ -z "$TMPVAR" ] || ZABBIX_DBPASSWORD="$( echo "$TMPVAR" | sed -r -e 's/^DBPassword=(.+)$/\1/' )"
 fi
 
-# if any of the vars is empty at this point, it means we're using the default value
-echo "+- ZABBIX_DBHOST: ${ZABBIX_DBHOST:='localhost'}"
-echo "+- ZABBIX_DBPORT: ${ZABBIX_DBPORT:='5432'}"
-echo "+- ZABBIX_DBHOST: ${ZABBIX_DBNAME:='zabbix'}"
-echo "+- ZABBIX_DBUSER: ${ZABBIX_DBUSER:='zabbix'}"
+# if any of the vars is unset at this point, it means we're using the default value
+ZABBIX_DBHOST="${ZABBIX_DBHOST:-localhost}"
+ZABBIX_DBPORT="${ZABBIX_DBPORT:-5432}"
+ZABBIX_DBNAME="${ZABBIX_DBNAME:-zabbix}"
+ZABBIX_DBUSER="${ZABBIX_DBUSER:-zabbix}"
 # yeah, the default password is empty. if it's set nothing will change, if it's not, it will get set to empty string
 ZABBIX_DBPASSWORD="$ZABBIX_DBPASSWORD"
+
+# inform
+echo "+- ZABBIX_DBHOST: $ZABBIX_DBHOST"
+echo "+- ZABBIX_DBPORT: $ZABBIX_DBPORT"
+echo "+- ZABBIX_DBNAME: ${ZABBIX_DBNAME:='zabbix'}"
+echo "+- ZABBIX_DBUSER: ${ZABBIX_DBUSER:='zabbix'}"
 
 # check if we have a database configured
 PGPASSWORD="$ZABBIX_DBPASSWORD"
